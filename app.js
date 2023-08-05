bb49b7416e54f5e193b65469b54811a0363a1430
Ira Oresta <108106124+IrOsens@users.noreply.github.com>
2023-08-05 14:15:55 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+client.on('message', async message => {
+    if (message.body === '!siapa kamu') {
+        client.sendMessage(message.from, 'Saya adalah bot punya rio 🥰');
+    }
+});
+
+// Mention everyone
+client.on('message', async (msg) => {
+    if(msg.body === '!everyone') {
+        const chat = await msg.getChat();
+        
+        let text = "";
+        let mentions = [];
+
+        for(let participant of chat.participants) {
+            const contact = await client.getContactById(participant.id._serialized);
+            
+            mentions.push(contact);
+            text += `@${participant.id.user} `;
+        }
+
+        await chat.sendMessage(text, { mentions });
+    }
+});
+
+// Mention contacts that send you a message
+/*
+client.on('message', async (msg) => {
+    const chat = await msg.getChat();
+    const contact = await msg.getContact();
+    
+    await chat.sendMessage(`Hello @${contact.id.user}`, {
+        mentions: [contact]
+    }); 
+});*/
